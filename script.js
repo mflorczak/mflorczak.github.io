f8cf3cc1a7b8927864cc9eb7352eb293b5a24751
mflorczak <michal.florczak0@gmail.com>
2018-06-25 19:47:26 +0200

    Update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://polar-ridge-73119.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://polar-ridge-73119.herokuapp.com/v1/trello/';
+  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
